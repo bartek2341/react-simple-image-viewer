4689782517bbdb14a3d4ff9c71361c72de1dd54d
Kot <bartek2341@o2.pl>
2022-12-08 15:58:41 +0100

    add player svg if video

--- src/ReactSimpleImageViewer.tsx
+++ src/ReactSimpleImageViewer.tsx
@@ -126,7 +126,8 @@ const ReactSimpleImageViewer = (props: IProps) => {
         onClick={handleClick}
       >
         <div className={`${styles.slide} react-simple-image-viewer__slide`}>
-          <img style={{cursor: "pointer"}} className={styles.image} src={props.src[currentIndex]} alt="" onClick={() => edge.opener()}/>
+          {edge.is_video && <img className={styles.svg} src={"./play.svg"} alt="play"/>}
+          <img className={styles.image} src={props.src[currentIndex]} alt="" onClick={() => edge.opener()}/>
         </div>
       </div>
     </div>
