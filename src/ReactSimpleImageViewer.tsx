0bf1ef995adac7ebf32329e3bc62e1d7cf6c1e1b
Kot <bartek2341@o2.pl>
2023-03-01 14:06:07 +0100

    add alternate img gallery

--- src/ReactSimpleImageViewer.tsx
+++ src/ReactSimpleImageViewer.tsx
@@ -125,7 +125,10 @@ const ReactSimpleImageViewer = (props: IProps) => {
         onClick={handleClick}
       >
         <div className={`${styles.slide} react-simple-image-viewer__slide`}>
-          <img className={styles.image} src={props.src[currentIndex]} alt="" onClick={() => edge.opener()}/>
+          <img className={styles.image} src={props.src[currentIndex]} alt="" onClick={() => edge.opener()} onError={({ currentTarget }) => {
+            currentTarget.onerror = null;
+            currentTarget.src = "./alternate.png";
+            }}/>
         </div>
       </div>
     </div>
